f5589efe94f59a27f40e3246327c5b7045d07575
shubhamkamble <kshub44kamble@gmail.com>
2020-06-13 11:25:17 +0530

    added: Flow Control Example using Python Programming.

---
+++ FLOW_CONTROL/whileloop/continueStmt.py
@@ -0,0 +1,10 @@
+while True:
+	print('Who are you?')
+	name = input()
+	if name!='Shubham':
+		continue
+	print('Hello,Shubham. What is the password?(It is a surname.)')
+	password = input()
+	if password == 'kamble':
+		break
+print('Access granted.')
---
+++ FLOW_CONTROL/whileloop/yourName2.py
@@ -0,0 +1,7 @@
+#break Statements
+while True:
+	print('Please type your name.')
+	name = input()
+	if name == 'your name':
+		break
+print('Thank you!')
