e6d58c714c5f16e053e160d002e348e5f73a504f
shubhamkamble <kshub44kamble@gmail.com>
2020-06-21 12:43:52 +0530

    added: List example duplicate code.

---
+++ LISTS/allMyCats1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#These programs also have a lot of duplicate or nearly identical code in them.
+print('Enter the name of cat1:')
+catName1 = input()
+print('Enter the name of cat2:')
+catName2 = input()
+print('Enter the name of cat3:')
+catName3 = input()
+print('Enter the name of cat4:')
+catName4 = input()
+print('Enter the name of cat5:')
+catName5 = input()
+print('Enter the name of cat6:')
+catName6 = input()
+print('cat names are:')
+print(catName1+''+catName2+''+catName3+''+catName4+''+catName5+''+catName6)
+
+
+
+
